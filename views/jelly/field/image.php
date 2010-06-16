8806a5bd89aa2fcc2bd4de764865da209b5a99d3
Kelvin Luck <git@kelvinluck.com>
2010-06-16 21:54:15 +0100

    Removing more of my custom code which is going into my application and out of Jelly... I guess with ->input() being deprecated then this won't be needed any more anyway in the core...

--- views/jelly/field/image.php
+++ views/jelly/field/image.php
@@ -1,16 +1,1 @@
-<?php
-	echo Form::file($name, $attributes + array('id' => 'field-'.$name));
-
-	// If there is an uploaded image then display it.
-	// Default to the first thumbnail if thumbnails exist
-	// (on the assumption this one is smaller and more suitable
-	// for displaying here)
-	if ($value != '' && !is_array($model->{$field->name})):
-		if (count($field->thumbnails)) {
-			$img_path = $field->thumbnails[0]['path'];
-		} else {
-			$img_path = $field->path;
-		}
-		echo Html::image(str_replace(DOCROOT.DIRECTORY_SEPARATOR, '', $img_path).$model->{$field->name});
-	endif;
-?>
+<?php echo Form::file($name, $attributes + array('id' => 'field-'.$name)); ?>
