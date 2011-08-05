614d682b14ca0b6f180754650132868b82c21b72
creatoro <vito@ahogysikerul.hu>
2011-08-05 13:15:40 +0200

    Fixes #66.

--- classes/jelly/core/field/enum.php
+++ classes/jelly/core/field/enum.php
@@ -26,38 +26,38 @@ abstract class Jelly_Core_Field_Enum extends Jelly_Field_String {
 	{
 		parent::__construct($options);
 
-		// Ensure we have choices to gather values from
 		if (empty($this->choices))
 		{
+			// Ensure we have choices to gather values from
 			throw new Kohana_Exception(':class must have a `choices` property set', array(
 				':class' => get_class($this)));
 		}
 
-		// Set allow_null to TRUE if we find a NULL value
 		if (in_array(NULL, $this->choices))
 		{
+			// Set allow_null to TRUE if we find a NULL value
 			$this->allow_null = TRUE;
 		}
-		// We're allowing NULLs but the value isn't set. Create it so validation won't fail.
 		elseif ($this->allow_null)
 		{
+			// We're allowing NULLs but the value isn't set. Create it so validation won't fail.
 			array_unshift($this->choices, NULL);
 		}
 
+		// Select the first choice
 		reset($this->choices);
 
-		// Convert non-associative values to associative ones
 		if ( ! Arr::is_assoc($this->choices))
 		{
+			// Convert non-associative values to associative ones
 			$this->choices = array_combine($this->choices, $this->choices);
 		}
 
-		// Set the default value from the first choice in the array
 		if ( ! array_key_exists('default', $options))
 		{
+			// Set the default value from the first choice in the array
 			$this->default = key($this->choices);
 
-			// Check if the associated key's value is NULL
 			if ($this->choices[$this->default] === NULL)
 			{
 				// Set the default to NULL instead of using the key which is an empty string for NULL values
@@ -67,6 +67,27 @@ abstract class Jelly_Core_Field_Enum extends Jelly_Field_String {
 
 		// Add a rule to validate that the value is proper
 		$this->rules[] = array('array_key_exists', array(':value', $this->choices));
+	}
+
+	/**
+	 * Selects the default value from the choices if NULLs are not allowed, but
+	 * the given value is NULL.
+	 *
+	 * This is intended to mimic the way MySQL selects the default value if
+	 * nothing is given.
+	 *
+	 * @param   mixed  $value
+	 * @return  string
+	 */
+	public function set($value)
+	{
+		if ($value === NULL AND ! $this->allow_null)
+		{
+			// Set value to the default value
+			$value = $this->default;
+		}
+
+		return parent::set($value);
 	}
 
 } // End Jelly_Core_Field_Enum
